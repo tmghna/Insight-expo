1195ae0f68d5675ed7b7c869296ba1b293574701
Gokul P Bharathan <gokulpbharathan89@gmail.com>
2025-04-13 12:08:35 +0530

    Firebase connection established

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { Constants } from "expo-constants";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD67VOJO5vunXFd-0f1Fo4JDV4Fv108Zxc",
+  authDomain: "insight-expo-62564.firebaseapp.com",
+  projectId: "insight-expo-62563",
+  storageBucket: "insight-expo-62564.firebasestorage.app",
+  messagingSenderId: "372122265926",
+  appId: "1:372122265926:web:9111431c03c271b9895d47",
+  firebaseMeasurementId: "G-V64VEZ4WC1"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
