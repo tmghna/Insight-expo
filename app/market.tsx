dbc589423a0db04c1c9cd318d09df963d89136be
Harshita <ms23216@iisermohali.ac.in>
2025-07-20 21:01:35 +0530

    Unresponsive Markets page

--- app/market.tsx
+++ app/market.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { PublicMarketSection } from "@/components/PublicMarketView";
@@ -10,55 +10,63 @@ export default function Market() {
   const styles = useStyles();
 
   return (
-    <SafeAreaView>
-      <View>
-        {/*Header Section*/}
-        <View style={styles.header}>
-          <TouchableOpacity style={styles.backButton}>
-            <MaterialIcons
-              name="arrow-back-ios"
-              style={styles.backButtonIcon}
-            />
-          </TouchableOpacity>
-          <Text style={styles.title}>Market</Text>
-        </View>
-
-        {/*User Section*/}
-        <View style={styles.userSection}>
-          <TouchableOpacity style={styles.cataloguesButton}>
-            <MaterialIcons name="library-books" style={styles.userButtonIcon} />
-            <Text style={styles.userButtonText}>Your Catalogues</Text>
-          </TouchableOpacity>
-          <View style={{ flexDirection: "column" }}>
-            <TouchableOpacity style={styles.savedItemsButton}>
+    <ImageBackground
+      source={require("../assets/images/MARKETBGGGG.png")}
+      style={{ flex: 1 }}
+    >
+      <SafeAreaView edges={["top"]} style={styles.background}>
+        <View style={styles.background}>
+          {/*Header Section*/}
+          <View style={styles.header}>
+            <TouchableOpacity style={styles.backButton}>
               <MaterialIcons
-                name="format-list-bulleted"
+                name="arrow-back-ios"
+                style={styles.backButtonIcon}
+              />
+            </TouchableOpacity>
+            <Text style={styles.title}>Market</Text>
+          </View>
+
+          {/*User Section*/}
+          <View style={styles.userSection}>
+            <TouchableOpacity style={styles.cataloguesButton}>
+              <MaterialIcons
+                name="library-books"
                 style={styles.userButtonIcon}
               />
-              <Text style={styles.userButtonText}>Items Saved</Text>
+              <Text style={styles.userButtonText}>Your Catalogues</Text>
             </TouchableOpacity>
-            <View style={styles.userSection}>
-              <TouchableOpacity style={styles.addInfoButton}>
-                <MaterialIcons name="phone" style={styles.userButtonIcon} />
-                <Text style={styles.userButtonText}>Contacts</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.addInfoButton}>
+            <View style={{ flexDirection: "column", gap: 5 }}>
+              <TouchableOpacity style={styles.savedItemsButton}>
                 <MaterialIcons
-                  name="playlist-add"
+                  name="format-list-bulleted"
                   style={styles.userButtonIcon}
                 />
-                <Text style={styles.userButtonText}>Add Items</Text>
+                <Text style={styles.userButtonText}>Items Saved</Text>
               </TouchableOpacity>
+              <View style={{ flexDirection: "row", gap: 5 }}>
+                <TouchableOpacity style={styles.addInfoButton}>
+                  <MaterialIcons name="phone" style={styles.userButtonIcon} />
+                  <Text style={styles.userButtonText}>Contacts</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.addInfoButton}>
+                  <MaterialIcons
+                    name="playlist-add"
+                    style={styles.userButtonIcon}
+                  />
+                  <Text style={styles.userButtonText}>Add Items</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
 
-        {/*Public Market Section*/}
-        <View style={styles.publicSection}>
-          <PublicMarketSection />
+          {/*Public Market Section*/}
+          <View style={styles.publicSection}>
+              <PublicMarketSection />
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
@@ -66,21 +74,99 @@ const useStyles = () => {
   return StyleSheet.create({
     background: {
       flexDirection: "column",
+      flex: 1,
+      color: "#1f1f1f",
+      justifyContent: "space-between",
     },
     header: {
       flexDirection: "row",
+      paddingHorizontal: 20,
+      paddingBottom: 10,
+      flex: 1,
+      height: "auto",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      marginVertical: 0,
     },
-    backButton: {},
-    backButtonIcon: {},
-    title: {},
+    backButton: {
+      color: "#00000000",
+      paddingVertical: 5,
+      paddingBottom: 7,
+      width: 25,
+      height: "auto",
+    },
+    backButtonIcon: {
+      color: "#f9f9f9ff",
+      fontSize: 22,
+    },
+    title: {
+      paddingStart: 20,
+      fontFamily: "Lato",
+      fontSize: 25,
+      fontWeight: 400,
+      letterSpacing: 0,
+      color: "#f9f9f9ff",
+    },
     userSection: {
       flexDirection: "row",
+      flex: 1,
+      paddingHorizontal: 20,
+      paddingBottom: 20,
+      marginBottom: 70,
+      gap: 5,
     },
-    cataloguesButton: {},
-    savedItemsButton: {},
-    addInfoButton: {},
-    userButtonIcon: {},
-    userButtonText: {},
-    publicSection: {},
+    cataloguesButton: {
+      width: "35%",
+      height: 80,
+      backgroundColor: "#1f1f1fb6",
+      borderRadius: 20,
+      flexDirection: "column",
+      padding: 8,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    savedItemsButton: {
+      width: "75%",
+      height: 35,
+      backgroundColor: "#1f1f1fb6",
+      borderRadius: 20,
+      flexDirection: "row",
+      padding: 4,
+      justifyContent: "space-evenly",
+      alignItems: "center",
+    },
+    addInfoButton: {
+      width: "37.5%",
+      height: 40,
+      backgroundColor: "#1f1f1fb6",
+      borderRadius: 20,
+      flexDirection: "row",
+      padding: 5,
+      justifyContent: "center",
+      alignItems: "center",
+      gap:2,
+    },
+    userButtonIcon: {
+      color: "#95a1ac",
+      fontSize: 24,
+    },
+    userButtonText: {
+      paddingVertical: 5,
+      textAlign: "center",
+      fontFamily: "WorkSans",
+      fontSize: 13,
+      fontWeight: 400,
+      letterSpacing: 0,
+      overflow: 'hidden',
+      color:"#fff",
+      alignSelf: 'center',
+    },
+    publicSection: {
+      width: "100%",
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      height: "65%",
+      backgroundColor: '#1f1f1f60',
+    },
   });
 };
--- components/PublicMarketView.tsx
+++ components/PublicMarketView.tsx
@@ -17,16 +17,36 @@ export function PublicMarketSection() {
       {/*Categories*/}
       <View style={styles.categoriesSection}>
         <TouchableOpacity style={styles.categories}>
-          <Image style={styles.appliancesImage} />
+          <Image
+            source={require("../assets/images/home-appliance.png")}
+            style={styles.categoryImage}
+          />
+          <Text style={styles.categoryName}>Appliances</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.categories}>
-          <Image style={styles.booksImage} />
+          <Image
+            source={require("../assets/images/open-book.png")}
+            style={styles.categoryImage}
+          />
+          <Text style={styles.categoryName}>Books</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.categories}>
-          <Image style={styles.furnitureImage} />
+          <Image
+            source={require("../assets/images/furniture.png")}
+            style={styles.categoryImage}
+          />
+          <Text style={styles.categoryName}>
+            Furniture{'\n'} & Decor
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.categories}>
-          <Image style={styles.utilityImage} />
+          <Image
+            source={require("../assets/images/yarn-ball.png")}
+            style={styles.categoryImage}
+          />
+          <Text style={styles.categoryName}>
+            Utility,{'\n'} Misc.
+          </Text>
         </TouchableOpacity>
       </View>
 
@@ -41,7 +61,7 @@ export function PublicMarketSection() {
 
       {/*Recent Items*/}
       <View style={styles.divider} />
-      <View style={styles.section}>
+      <View style={styles.recentSection}>
         <View style={styles.sectionHeader}>
           <Text style={styles.sectionHeading}>Recent Items</Text>
           <Text style={styles.more}>See all</Text>
@@ -53,28 +73,76 @@ export function PublicMarketSection() {
 
 const useStyles = () => {
   return StyleSheet.create({
-    container: {},
+    container: {
+      width: "100%",
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingHorizontal: 10,
+      paddingVertical: 10,
+    },
     startMark: {
       flexDirection: "row",
+      width: 50,
+      height: 3,
+      backgroundColor: "#6c6a6a41",
+      alignSelf: "center",
+      marginBottom: 10,
     },
     categoriesSection: {
       flexDirection: "row",
+      justifyContent: "space-evenly",
     },
-    categories: {},
-    appliancesImage: {},
-    booksImage: {},
-    furnitureImage: {},
-    utilityImage: {},
+    categories: {
+      flexDirection: "column",
+      paddingVertical: 15,
+      justifyContent: "space-evenly",
+      gap: 5,
+      alignItems: "center",
+      width: "17.5%",
+    },
+    categoryImage: {
+      width: 40,
+      height: 40,
+    },
+    categoryName: {
+      fontFamily: "WorkSans",
+      fontSize: 12,
+      fontWeight: 400,
+      letterSpacing: 0,
+      color: "#fff",
+    },
     divider: {
       flexDirection: "row",
+      width: 350,
+      height: 1,
+      backgroundColor: "#6c6a6a41",
     },
     section: {
       flexDirection: "column",
+      height: 150,
+      paddingHorizontal: 20,
     },
     sectionHeader: {
       flexDirection: "row",
+      justifyContent: "space-between",
+      paddingTop: 10,
     },
-    sectionHeading: {},
-    more: {},
+    sectionHeading: {
+      fontFamily: "WorkSans",
+      fontSize: 14,
+      fontWeight: 400,
+      color: "#95a1ac",
+    },
+    more: {
+      fontFamily: "WorkSans",
+      fontSize: 12,
+      fontWeight: 400,
+      color: "#95a1ac",
+    },
+    recentSection: {
+      flexDirection: "column",
+      height: 800,
+      paddingHorizontal: 20,
+    },
   });
 };
